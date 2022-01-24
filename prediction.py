fde3cabbced9d4203acec40cf5fb60aa0cc4d5a3
Eleonora <eleonora.peruch@studio.unibo.it>
2022-01-24 18:30:07 +0000

    fraud detection application
    Created model for fraud detection

--- prediction.py
+++ prediction.py
@@ -1,10 +1,15 @@
 # For sample predict functions for popular libraries visit https://github.com/opendatahub-io/odh-prediction-samples
 
 # Import libraries
-# import tensorflow as tf
+import tensorflow as tf
+#from tensorflow import keras
+import pandas as pd
+import numpy as np
+#import pickle
+
 
 # Load your model.
-# model_dir = 'models/myfancymodel'
+#model_dir = 'models/fraudmodel.json'
 # saved_model = tf.saved_model.load(model_dir)
 # predictor = saved_model.signatures['default']
 
@@ -15,3 +20,11 @@ def predict(args_dict):
 #     predictor(arg)
     return {'prediction': 'not implemented'}
 
+
+
+
+#def predict(sample_transaction):
+#    #data['Class'].values
+
+#    single_prediction = xgb.predict(sample_transaction)
+#    return {'prediction': single_prediction}
